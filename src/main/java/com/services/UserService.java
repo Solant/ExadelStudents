518f3869de6eda55dee160d0c0172498063bd471
Victor <runner62v6@gmail.com>
2014-07-25 15:27:41 +0300

    Merge with master, some services, documentation

---
+++ src/main/java/com/services/UserService.java
@@ -0,0 +1,23 @@
+package com.services;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class UserService {
+    public static boolean isLoginAvailable(String login){
+        //Some code missed, lol
+        //Get from DAO findUserByLogin
+        return true;
+    }
+
+    /**
+     * Returns login of current user from SpringSecurity
+     *
+     * @return String
+     */
+    public static String getCurrentUserLogin(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        return name;
+    }
+}
