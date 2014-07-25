d245452f5ba629c3b1345850e141a5f19298020c
Victor <runner62v6@gmail.com>
2014-07-25 15:43:46 +0300

    StudentService add realisation

--- src/main/java/com/services/StudentService.java
+++ src/main/java/com/services/StudentService.java
@@ -1,16 +1,44 @@
 package com.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import persistance.dao.StudentDao;
 import persistance.model.Student;
+import persistance.model.UserRole;
 
 import java.util.*;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    private static StudentDao studentDao;
+
+    /**
+     * Creates new student
+     *
+     * @param login - Student login
+     * @param password - Student password
+     * @param name - Student name
+     * @param surname - Student second name
+     *
+     */
     @Transactional
-    static public void add(String login, String password, String surname, String name){
-        //Adding Student
+    static public void add(String login, String password, String name, String surname){
+        Student student = new Student();
+
+        student.setLogin(login);
+        student.setPassword(password);
+        student.setFirstName(name);
+        student.setSecondName(surname);
+
+        UserRole ur = new UserRole();
+        ur.setRole("ROLE_STUDENT");
+        ur.setUser(student);
+        student.getUserRoles().add(ur);
+
+        studentDao.save(student);
     }
 
     @Transactional
