d8d42817c5d4a8190e0d72b8ef59021318a654c7
disparate <disparateblr@mail.ru>
2014-07-29 10:38:12 +0300

    groupDao.getByStatus,  adminDao.getByLogin realisations

--- src/main/java/persistance/dao/GroupDao.java
+++ src/main/java/persistance/dao/GroupDao.java
@@ -2,5 +2,8 @@ package persistance.dao;
 
 import persistance.model.Group;
 
+import java.util.List;
+
 public interface GroupDao extends GenericDao<Group> {
+    public List<Group> getByStatus(String status);
 }
