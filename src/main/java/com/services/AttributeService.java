0b3ba12da169c047688ca4b86b5493bc67dce0f8
Victor <runner62v6@gmail.com>
2014-07-30 18:45:36 +0300

    getAllAtributes

--- src/main/java/com/services/AttributeService.java
+++ src/main/java/com/services/AttributeService.java
@@ -1,5 +1,6 @@
 package com.services;
 
+import com.services.presentation.GAVPresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +9,11 @@ import persistance.dao.AttributeDao;
 import persistance.dao.GroupDao;
 import persistance.model.Attribute;
 import persistance.model.Group;
+import persistance.model.Value;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class AttributeService {
@@ -30,5 +36,23 @@ public class AttributeService {
     @Transactional
     public void removeAttribute(String attributeName){
         attributeDao.removeById(attributeDao.findByName(attributeName).getId());
+    }
+
+    @Transactional
+    public List<GAVPresentation> getAll(){
+        List<Group> groups = groupDao.findAll();
+        List<GAVPresentation> gavs = new ArrayList<GAVPresentation>();
+        for(Group group : groups){
+            Set<Attribute> attributes = group.getAttributes();
+            for(Attribute attribute : attributes){
+                Set<Value> values = attribute.getValues();
+                GAVPresentation gav = new GAVPresentation();
+                gav.setGroup(group.getName());
+                gav.setAttribute(attribute.getAttributeName());
+                gav.setType(attribute.getType());
+                gavs.add(gav);
+            }
+        }
+        return gavs;
     }
 }
