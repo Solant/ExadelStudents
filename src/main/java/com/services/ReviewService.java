5408b6d3d0a44eaf5645fa001e0dee4a79b348cf
Victor <runner62v6@gmail.com>
2014-08-06 20:40:47 +0300

    nadoelo

---
+++ src/main/java/com/services/ReviewService.java
@@ -0,0 +1,19 @@
+package com.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import persistance.dao.ReviewDao;
+import persistance.model.Review;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewDao reviewDao;
+
+    @Transactional
+    public Review getReviewById(long id){
+        return reviewDao.findById(id);
+    }
+}
