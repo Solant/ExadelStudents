0fdf1d5f5e5ed1fe325705e38ad574b7359fbf27
disparate <disparateblr@mail.ru>
2014-08-08 14:34:29 +0300

    JSONStudent class

---
+++ src/main/java/com/forView/JSONStudent.java
@@ -0,0 +1,34 @@
+package com.forView;
+
+/**
+ * Created by user on 08.08.2014.
+ */
+public class JSONStudent {
+    private String firstName;
+    private String secondName;
+    private String login;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
